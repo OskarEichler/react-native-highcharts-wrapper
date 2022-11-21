0c0005d9c5705ad24c4b1be1de352c7b596bf6bb
Oskar Eichler <62393985+OskarEichler@users.noreply.github.com>
2022-11-21 16:47:50 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
 import { View } from 'react-native';
 
 import styles from './styles';
 import { getInit, getEnd, flattenObject } from './helpers';
 
-function Chart(props) {
-  let config = JSON.stringify(props.config, (_, value) => {
-    //create string of json but if it detects function it uses toString()
-    return typeof value === 'function' ? value.toString() : value;
-  });
+class Chart extends Component {
 
-  config = JSON.parse(config);
-  const concatHTML = `${getInit(props)}${flattenObject(config)}${getEnd()}`;
+  shouldComponentUpdate(nextProps) {
+    return (this.props.nameForReload !== nextProps.nameForReload); //Using this prop to tell when to re-render
+  }
 
-  return (
-    <View style={props.style}>
-      <WebView
-        style={styles.full}
-        source={{ html: concatHTML, baseUrl: 'web/' }}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        scalesPageToFit={true}
-        scrollEnabled={false}
-        automaticallyAdjustContentInsets={true}
-        {...props}
-      />
-    </View>
-  );
+  render() {
+    let config = JSON.stringify(this.props.config, (_, value) => {
+      //create string of json but if it detects function it uses toString()
+      return typeof value === 'function' ? value.toString() : value;
+    });
+
+    config = JSON.parse(config);
+    const concatHTML = `${getInit(this.props)}${flattenObject(config)}${getEnd()}`;
+
+    return (
+      <View style={this.props.style}>
+        <WebView
+          style={styles.full}
+          source={{ html: concatHTML, baseUrl: 'web/' }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          scalesPageToFit={true}
+          scrollEnabled={false}
+          automaticallyAdjustContentInsets={true}
+          {...this.props}
+        />
+      </View>
+    );
+  }
 }
 
-export default Chart;
+export default function ChartContainer(props) {
+  return <Chart {...props} />
+}
